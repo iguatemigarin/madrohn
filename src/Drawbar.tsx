5fbdc0614b8c7fcac14fcfacfa70665dec787645
jandreo <jonathan.andreo@soundcloud.com>
2021-10-08 15:01:52 +0200

    Dont blow your ears off

--- src/Drawbar.tsx
+++ src/Drawbar.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { audioContext, masterGain } from "./globals";
+import { masterGain } from "./globals";
 import "./Drawbar.css";
 
 type VolumeProps = {
@@ -20,14 +20,23 @@ type DrawbarProps = {
 // TODO get from computed style
 const organMaxHeight = 300;
 
+const calculateVolume = (offsetY: number) => {
+  // const perceptionProportion = 1 / ((index + 1) * 1.1)
+  return Math.min(
+    ((organMaxHeight - offsetY) / organMaxHeight),
+    1
+  )
+}
+
 export const Drawbar: React.FC<DrawbarProps> = ({ note, rootNote, paint, oscillator }) => {
   const [volume, setVolume] = React.useState(0)
-  console.log('volume', volume);
-  React.useEffect(() => {
-    setVolume(volume)
-    oscillator.oscGain.gain.value = volume
-    oscillator.oscGain.connect(masterGain)
-  }, [volume])
+
+  // ! BE VERY CAREFUL WHEN UNCOMMENTING THIS SHIT, SHIT CAN GET LOUD
+  // React.useEffect(() => {
+  //   console.log('volume', volume);
+  //   oscillator.oscGain.gain.value = volume
+  //   oscillator.oscGain.connect(masterGain)
+  // }, [volume])
   return (
     <div
       className={`Drawbar ${rootNote ? "m-root" : ""}`}
